d2850a7f80f69a62ce84c8955786ea5a3dae737a
wesleyara <wes1gom@gmail.com>
2024-08-10 09:27:59 -0300

    feat: add prisma service and start configs

--- src/infra/infra.module.ts
+++ src/infra/infra.module.ts
@@ -1,9 +1,11 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { EnvConfigModule } from './env-config/env-config.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [EnvConfigModule.forRoot(), HttpModule],
+  providers: [PrismaService],
   exports: [HttpModule],
 })
 export class InfraModule {}
