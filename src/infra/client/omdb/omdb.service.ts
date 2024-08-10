38aa4987cc46fef1843ab900385b4cfdb175289e
wesleyara <wes1gom@gmail.com>
2024-08-10 11:51:22 -0300

    feat: add OMDB interface for search by title response

--- src/infra/client/omdb/omdb.service.ts
+++ src/infra/client/omdb/omdb.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { HttpClientService } from 'src/infra/http/http.service';
+import { SearchByTitleResponse } from './interface/omdb.interface';
 
 @Injectable()
 export class OmdbService {
   constructor(private readonly httpClient: HttpClientService) {}
 
   async searchByTitle(title: string) {
-    return this.httpClient.get(`http://www.omdbapi.com/?t=${title}&apikey=${process.env.OMDB_API_KEY}`);
+    return this.httpClient.get<SearchByTitleResponse>(`http://www.omdbapi.com/?t=${title}&apikey=${process.env.OMDB_API_KEY}`);
   }
 }
