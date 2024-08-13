9d38723084773d9a22520b3f7d2e29ccaed6825d
wesleyara <wes1gom@gmail.com>
2024-08-13 17:31:46 -0300

    fix: removing prisma code

--- src/infra/infra.module.ts
+++ src/infra/infra.module.ts
@@ -1,12 +1,11 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { EnvConfigModule } from './env-config/env-config.module';
-import { PrismaService } from './prisma/prisma.service';
 import { ClientModule } from './client/client.module';
 
 @Module({
   imports: [EnvConfigModule.forRoot(), HttpModule, ClientModule],
-  providers: [PrismaService],
+  providers: [],
   exports: [HttpModule],
 })
 export class InfraModule {}
